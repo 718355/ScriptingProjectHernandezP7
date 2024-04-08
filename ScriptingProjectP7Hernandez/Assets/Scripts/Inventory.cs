7ae5f74e265fea164060ac336003c2bb5b7b4baa
718355 <718355@student.dusd.net>
2024-04-08 15:27:55 -0700

    Haven't finished the scripts.

---
+++ ScriptingProjectP7Hernandez/Assets/Scripts/Inventory.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public class Stuff
+    {
+        public int bullets;
+        public int grenades;
+        public int rockets;
+        public float fuel;
+    }
+
+    public Stuff(int bul, float fu)
+    {
+        bullets = bul;
+        grenades = CullingGroupEvent
+        rockets = 
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
